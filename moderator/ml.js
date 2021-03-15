3b180470ca2f205ad4fe4d4f01031e82d562d6b1
streambytes <streambytes@protonmail.ch>
2021-03-15 21:28:39 +0100

    train_test_split func added

--- moderator/ml.js
+++ moderator/ml.js
@@ -1,4 +1,4 @@
-const { Console } = require('console');
+const seedrandom = require('seedrandom');
 const parse = require('csv-parse/lib/sync');
 const tfjs = require('@tensorflow/tfjs');
 const keras = require('keras-js');
@@ -6,7 +6,7 @@ const fs = require('fs').promises;
 
 class tokenizer {
     constructor(dataset) {
-        this.vocab = [];
+        this.vocab = ["ksdafpdsngpdnagpkenpgneapngepng"];
         this.buildVocab(dataset)
     }
 
@@ -46,14 +46,7 @@ class tokenizer {
     texts_to_sequences(texts, maxlen=250) {
         let seq = []
         texts.forEach(text => {
-            let x = this.text_to_sequence(text);
-            if (x.length > maxlen) x.substr(0, maxlen);
-            let padding = maxlen - x.length;
-
-            for (let i = 0; i < padding; i++) {
-                x.push(0);
-            }
-
+            let x = this.text_to_sequence(text, maxlen);
             seq.push(x);
         });
 
@@ -61,23 +54,63 @@ class tokenizer {
     }
 }
 
+function train_test_split(dataset, seed="$$$", maxlen=1000, test_size=0.25) {
+    // shuffle dataset
+    dataset.sort(() => { 
+        if (seed === "$$$")
+            return;
+        else {
+            let rng = seedrandom(); 
+            return 0.5 - (rng());
+        }
+    });
+
+    let n_test = (maxlen / test_size) * 100;
+    let n_train = maxlen - n_test; 
+
+    let train_els = dataset.slice(0, n_train);
+    let test_els = dataset.slice(n_train, n_test);
+
+    let obj = {
+        X_train : [],
+        X_test : [],
+        y_train : [],
+        y_test : []    
+    }
+
+    train_els.forEach((el) => {
+        obj['X_train'].push(el['tweet']);
+        obj['y_train'].push(el['label']);
+    });
+
+    test_els.forEach((el) => {
+        obj['X_test'].push(el['tweet']);
+        obj['y_test'].push(el['label']);
+    });
+
+    return obj;
+}
+
 (async function () {
+    // let time = Math.floor(Date.now() / 1000);
     const fileContent = await fs.readFile('dataset/labeled_data.csv');
     const dataset = parse(fileContent, {columns : true});
 
     for (let i = 0; i < dataset.length; i++) {
         let line = {
             class : (dataset[i]['class'] == 2) ? 1 : 0,
-            tweet : dataset[i]['tweet'].substr(1,dataset[i]['tweet'].length - 1)
+            tweet : dataset[i]['tweet']
         }
         dataset[i] = line;
     }
 
-    var tk = new tokenizer(dataset);
+    let data = train_test_split(dataset);
 
-    s = "This is just a test!";
+    let tk = new tokenizer(dataset);
 
-    var seq = tk.text_to_sequence(s, 10);
+    let X_train = tk.texts_to_sequences(data.X_train, padding=50);
+    let X_test = tk.texts_to_sequences(data.X_test, padding=50);
+    // time = Math.floor(Date.now() / 1000) - time;
 
-    console.log(seq);
+    //console.log(`Process ended in ${time} seconds`);
 })();
